52f680144b499143ab4627e20e415a68b519cfa9
Lovejeet Singh Parihar <lovysparihar@gmail.com>
2023-02-21 15:01:26 +0530

    web socket support added to the patient apps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'healthcareApp';
+
+  private serverUrl = 'http://localhost:8080/patientSocket'
+  private stompClient!: Stomp.Client;
+
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection(){
+    let ws: WebSocket = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    that.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/notify", (message: any) => {
+        that.onMessageReceived(message);
+      });
+    });
+  }
+
+  onMessageReceived(message: any) {
+    console.log(message);
+  }
 }
