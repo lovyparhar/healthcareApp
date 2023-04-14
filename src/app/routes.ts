2e8efde30670adda26b17906dd00800b73024717
Sukhamjot-Singh <sukhamjot2001@gmail.com>
2023-04-15 03:02:39 +0530

    added auth gaurd

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -9,18 +9,19 @@ import { ChangePasswordComponent } from './change-password/change-password.compo
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ResetPasswordOTPComponent } from './reset-password-otp/reset-password-otp.component';
 import { EditApproveConsentComponent } from './edit-approve-consent/edit-approve-consent.component';
+import { AuthGuard } from './auth.gaurd';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: HomeComponent },
   { path: '', component: HomeComponent },
-  { path: 'medicalrecords', component: MedicalRecordsComponent },
-  { path: 'composeconsent', component: ComposeConsentComponent },
-  { path: 'pendingconsents', component: PendingConsentsComponent },
+  { path: 'medicalrecords', component: MedicalRecordsComponent, canActivate: [AuthGuard]  },
+  { path: 'composeconsent', component: ComposeConsentComponent, canActivate: [AuthGuard]  },
+  { path: 'pendingconsents', component: PendingConsentsComponent, canActivate: [AuthGuard]  },
   { path: 'register', component: PatientRegisterComponent },
-  { path: 'dashboard', component: PatientDashboardComponent },
+  { path: 'dashboard', component: PatientDashboardComponent, canActivate: [AuthGuard]  },
   { path: 'change_password', component: ChangePasswordComponent },
   { path: 'reset_password', component: ResetPasswordComponent },
   { path: 'verify_reset_otp', component: ResetPasswordOTPComponent },
-  { path: 'editapproveconsent', component: EditApproveConsentComponent },
+  { path: 'editapproveconsent', component: EditApproveConsentComponent, canActivate: [AuthGuard]  },
 ];
